633878a9ccc7870ec19ef7c6f097fe4332043d13
demi0504 <ripley.demi@gmail.com>
2020-07-21 17:51:16 -0500

    final commit of the day

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+        customerName: 'Bob',
+        customerEmail: 'bob@example.com',
+        customerID: 'bob89',
+        phoneNumber: '000-000-0000'
+    }
+];
+
+module.exports = tableArray;
