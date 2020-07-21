5ccc04eccdda562046f0f9d15e7db0f92d851089
demi0504 <ripley.demi@gmail.com>
2020-07-21 15:59:36 -0500

    adding script to html files

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -19,7 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+require('./app/routing/api-routes.js')(app);
 require('./app/routing/html-routes.js')(app);
+
+
 
 // Starts the server to begin listening
 // =============================================================
